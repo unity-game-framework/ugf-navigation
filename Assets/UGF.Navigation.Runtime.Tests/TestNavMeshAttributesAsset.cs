67c51c83f9e8d3ca3df120ee1f451a27f375fb6d
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-10-11 18:27:23 +0300

    Add tests

---
+++ Assets/UGF.Navigation.Runtime.Tests/TestNavMeshAttributesAsset.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace UGF.Navigation.Runtime.Tests
+{
+    [CreateAssetMenu(menuName = "Tests/TestNavMeshAttributesAsset")]
+    public class TestNavMeshAttributesAsset : ScriptableObject
+    {
+        [NavMeshAgentType]
+        [SerializeField] private int m_agentType;
+        [NavMeshAreaMask]
+        [SerializeField] private int m_areaMask;
+
+        public int AgentType { get { return m_agentType; } set { m_agentType = value; } }
+        public int AreaMask { get { return m_areaMask; } set { m_areaMask = value; } }
+    }
+}
