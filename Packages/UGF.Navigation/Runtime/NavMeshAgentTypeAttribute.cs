eac2bf8f554e684e18e6d790e2eaf8f2a25d9d4a
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-10-11 18:27:03 +0300

    Add attributes

---
+++ Packages/UGF.Navigation/Runtime/NavMeshAgentTypeAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace UGF.Navigation.Runtime
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class NavMeshAgentTypeAttribute : PropertyAttribute
+    {
+    }
+}
