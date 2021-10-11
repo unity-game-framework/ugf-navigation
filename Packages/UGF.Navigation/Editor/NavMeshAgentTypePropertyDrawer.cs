ad05c23784c117a4bb51a397c7ed1ebe7198a537
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-10-11 18:27:13 +0300

    Add attribute drawers

---
+++ Packages/UGF.Navigation/Editor/NavMeshAgentTypePropertyDrawer.cs
@@ -0,0 +1,31 @@
+﻿using UGF.EditorTools.Editor.IMGUI.PropertyDrawers;
+using UGF.Navigation.Runtime;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace UGF.Navigation.Editor
+{
+    [CustomPropertyDrawer(typeof(NavMeshAgentTypeAttribute), true)]
+    internal class NavMeshAgentTypePropertyDrawer : PropertyDrawerTyped<NavMeshAgentTypeAttribute>
+    {
+        public NavMeshAgentTypePropertyDrawer() : base(SerializedPropertyType.Integer)
+        {
+        }
+
+        protected override void OnDrawProperty(Rect position, SerializedProperty serializedProperty, GUIContent label)
+        {
+            var labels = new GUIContent[NavMesh.GetSettingsCount()];
+
+            for (int i = 0; i < labels.Length; i++)
+            {
+                NavMeshBuildSettings settings = NavMesh.GetSettingsByIndex(i);
+                string name = NavMesh.GetSettingsNameFromID(settings.agentTypeID);
+
+                labels[i] = new GUIContent(name);
+            }
+
+            serializedProperty.intValue = EditorGUI.Popup(position, label, serializedProperty.intValue, labels);
+        }
+    }
+}
---
+++ Packages/UGF.Navigation/Editor/NavMeshAreaMaskPropertyDrawer.cs
@@ -0,0 +1,22 @@
+﻿using UGF.EditorTools.Editor.IMGUI.PropertyDrawers;
+using UGF.Navigation.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.Navigation.Editor
+{
+    [CustomPropertyDrawer(typeof(NavMeshAreaMaskAttribute), true)]
+    internal class NavMeshAreaMaskPropertyDrawer : PropertyDrawerTyped<NavMeshAreaMaskAttribute>
+    {
+        public NavMeshAreaMaskPropertyDrawer() : base(SerializedPropertyType.Integer)
+        {
+        }
+
+        protected override void OnDrawProperty(Rect position, SerializedProperty serializedProperty, GUIContent label)
+        {
+            string[] names = GameObjectUtility.GetNavMeshAreaNames();
+
+            serializedProperty.intValue = EditorGUI.MaskField(position, label, serializedProperty.intValue, names);
+        }
+    }
+}
