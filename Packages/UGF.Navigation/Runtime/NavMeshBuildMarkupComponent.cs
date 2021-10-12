7ef3d5841eed616c66e94557687b93288dc779f3
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-10-12 15:44:27 +0300

    Add NavMeshBuildMarkupComponent

---
+++ Packages/UGF.Navigation/Runtime/NavMeshBuildMarkupComponent.cs
@@ -0,0 +1,30 @@
+﻿using UGF.Builder.Runtime;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace UGF.Navigation.Runtime
+{
+    [AddComponentMenu("Unity Game Framework/Navigation/NavMesh Build Markup", 2000)]
+    public class NavMeshBuildMarkupComponent : BuilderComponent<NavMeshBuildMarkup>
+    {
+        [SerializeField] private bool m_overrideArea;
+        [NavMeshArea]
+        [SerializeField] private int m_area;
+        [SerializeField] private bool m_ignoreFromBuild;
+
+        public bool OverrideArea { get { return m_overrideArea; } set { m_overrideArea = value; } }
+        public int Area { get { return m_area; } set { m_area = value; } }
+        public bool IgnoreFromBuild { get { return m_ignoreFromBuild; } set { m_ignoreFromBuild = value; } }
+
+        protected override NavMeshBuildMarkup OnBuild()
+        {
+            return new NavMeshBuildMarkup
+            {
+                overrideArea = m_overrideArea,
+                area = m_area,
+                ignoreFromBuild = m_ignoreFromBuild,
+                root = transform
+            };
+        }
+    }
+}
