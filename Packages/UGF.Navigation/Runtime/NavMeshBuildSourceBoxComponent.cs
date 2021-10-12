79678eecc5cd012034de3f85402dc5445b126b03
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-10-12 16:00:46 +0300

    Add NavMeshBuildSourceBoxComponent

---
+++ Packages/UGF.Navigation/Runtime/NavMeshBuildSourceBoxComponent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace UGF.Navigation.Runtime
+{
+    [AddComponentMenu("Unity Game Framework/Navigation/NavMesh Build Source Box", 2000)]
+    public class NavMeshBuildSourceBoxComponent : NavMeshBuildSourceComponent
+    {
+        [SerializeField] private Bounds m_bounds;
+
+        public Bounds Bounds { get { return m_bounds; } set { m_bounds = value; } }
+
+        protected override NavMeshBuildSource OnBuild()
+        {
+            Bounds bounds = NavigationUtility.GetWorldBounds(m_bounds, Vector3.zero, Quaternion.identity, Vector3.one);
+
+            return new NavMeshBuildSource
+            {
+                area = Area,
+                shape = NavMeshBuildSourceShape.Box,
+                transform = Matrix4x4.TRS(m_bounds.center, transform.rotation, Vector3.one),
+                size = bounds.size,
+                component = this
+            };
+        }
+    }
+}
