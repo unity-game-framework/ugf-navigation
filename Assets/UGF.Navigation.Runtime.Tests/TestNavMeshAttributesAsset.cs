c8e170bcf3caa5a594bfc147f8f1e3d8fc573942
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-10-12 15:23:51 +0300

    Add tests

--- Assets/UGF.Navigation.Runtime.Tests/TestNavMeshAttributesAsset.cs
+++ Assets/UGF.Navigation.Runtime.Tests/TestNavMeshAttributesAsset.cs
@@ -9,8 +9,11 @@ namespace UGF.Navigation.Runtime.Tests
         [SerializeField] private int m_agentType;
         [NavMeshAreaMask]
         [SerializeField] private int m_areaMask;
+        [NavMeshArea]
+        [SerializeField] private int m_area;
 
         public int AgentType { get { return m_agentType; } set { m_agentType = value; } }
         public int AreaMask { get { return m_areaMask; } set { m_areaMask = value; } }
+        public int Area { get { return m_area; } set { m_area = value; } }
     }
 }
