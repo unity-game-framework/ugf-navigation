2eb8abca989d31438ec627b8dcf50167c1888d51
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-10-14 15:58:26 +0300

    Add NavMeshDataComponent and NavMeshLinkDataComponent

---
+++ Packages/UGF.Navigation/Runtime/NavMeshDataComponent.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace UGF.Navigation.Runtime
+{
+    [AddComponentMenu("Unity Game Framework/Navigation/NavMesh Data", 2000)]
+    public class NavMeshDataComponent : MonoBehaviour
+    {
+        [SerializeField] private NavMeshData m_data;
+
+        public NavMeshData Data { get { return m_data; } set { m_data = value; } }
+        public NavMeshDataInstance Instance { get { return m_instance ?? throw new ArgumentException("Value not specified."); } }
+        public bool HasInstance { get { return m_instance != null; } }
+
+        private NavMeshDataInstance? m_instance;
+
+        public void Add()
+        {
+            if (m_instance != null) throw new InvalidOperationException("NavMesh Data already added.");
+
+            m_instance = NavMesh.AddNavMeshData(m_data);
+        }
+
+        public bool Remove()
+        {
+            if (m_instance != null)
+            {
+                NavMesh.RemoveNavMeshData(m_instance.Value);
+
+                m_instance = null;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private void OnEnable()
+        {
+            if (m_instance == null)
+            {
+                Add();
+            }
+        }
+
+        private void OnDisable()
+        {
+            Remove();
+        }
+    }
+}
---
+++ Packages/UGF.Navigation/Runtime/NavMeshLinkDataComponent.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace UGF.Navigation.Runtime
+{
+    [AddComponentMenu("Unity Game Framework/Navigation/NavMesh Link Data", 2000)]
+    public class NavMeshLinkDataComponent : MonoBehaviour
+    {
+        [SerializeField] private NavMeshLinkComponent m_data;
+
+        public NavMeshLinkComponent Data { get { return m_data; } set { m_data = value; } }
+        public NavMeshLinkInstance Instance { get { return m_instance ?? throw new ArgumentException("Value not specified."); } }
+        public bool HasInstance { get { return m_instance != null; } }
+
+        private NavMeshLinkInstance? m_instance;
+
+        public void Add()
+        {
+            if (m_instance != null) throw new InvalidOperationException("NavMesh Link Data already added.");
+
+            NavMeshLinkData data = m_data.Build();
+
+            m_instance = NavMesh.AddLink(data);
+        }
+
+        public bool Remove()
+        {
+            if (m_instance != null)
+            {
+                NavMesh.RemoveLink(m_instance.Value);
+
+                m_instance = null;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private void OnEnable()
+        {
+            if (m_instance == null)
+            {
+                Add();
+            }
+        }
+
+        private void OnDisable()
+        {
+            Remove();
+        }
+    }
+}
