567ae0e3bfab5d46206dcc07bdbe3cdf2cf4e039
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-10-14 15:33:05 +0300

    Add NavMeshLinkComponent

---
+++ Packages/UGF.Navigation/Runtime/NavMeshLinkComponent.cs
@@ -0,0 +1,42 @@
+﻿using UGF.Builder.Runtime;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace UGF.Navigation.Runtime
+{
+    [AddComponentMenu("Unity Game Framework/Navigation/NavMesh Link", 2000)]
+    public class NavMeshLinkComponent : BuilderComponent<NavMeshLinkData>
+    {
+        [NavMeshArea]
+        [SerializeField] private int m_area;
+        [NavMeshAgentType]
+        [SerializeField] private int m_agentType;
+        [SerializeField] private Vector3 m_start;
+        [SerializeField] private Vector3 m_end = Vector3.forward;
+        [SerializeField] private float m_width = 1F;
+        [SerializeField] private float m_cost = 1F;
+        [SerializeField] private bool m_bidirectional = true;
+
+        public int Area { get { return m_area; } set { m_area = value; } }
+        public int AgentType { get { return m_agentType; } set { m_agentType = value; } }
+        public Vector3 Start { get { return m_start; } set { m_start = value; } }
+        public Vector3 End { get { return m_end; } set { m_end = value; } }
+        public float Width { get { return m_width; } set { m_width = value; } }
+        public float Cost { get { return m_cost; } set { m_cost = value; } }
+        public bool Bidirectional { get { return m_bidirectional; } set { m_bidirectional = value; } }
+
+        protected override NavMeshLinkData OnBuild()
+        {
+            return new NavMeshLinkData
+            {
+                area = 0,
+                agentTypeID = 0,
+                startPosition = Vector3.zero,
+                endPosition = Vector3.zero,
+                width = 0F,
+                costModifier = 0F,
+                bidirectional = false
+            };
+        }
+    }
+}
