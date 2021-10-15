d9435d261608d1688a0be2ded537e9ca08776189
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-10-15 19:10:31 +0300

    Update NavMeshDataComponent.cs

--- Packages/UGF.Navigation/Runtime/NavMeshDataComponent.cs
+++ Packages/UGF.Navigation/Runtime/NavMeshDataComponent.cs
@@ -21,7 +21,7 @@ namespace UGF.Navigation.Runtime
         {
             if (m_instance != null) throw new InvalidOperationException("NavMesh Data already added.");
 
-            m_instance = NavMesh.AddNavMeshData(m_data, transform.position, transform.rotation);
+            m_instance = NavMesh.AddNavMeshData(m_data);
         }
 
         public bool Remove()
