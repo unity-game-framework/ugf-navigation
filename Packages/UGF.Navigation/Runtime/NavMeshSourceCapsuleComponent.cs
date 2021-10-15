75bb680da85e73c0bc20dc4bdfa161e4a1f2d66a
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-10-15 21:20:27 +0300

    Add NavMeshSourceCapsuleComponent and NavMeshSourceSphereComponent

---
+++ Packages/UGF.Navigation/Runtime/NavMeshSourceCapsuleComponent.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace UGF.Navigation.Runtime
+{
+    [AddComponentMenu("Unity Game Framework/Navigation/NavMesh Source Capsule", 2000)]
+    public class NavMeshSourceCapsuleComponent : NavMeshSourceComponent
+    {
+        [SerializeField] private Vector3 m_center;
+        [SerializeField] private float m_radius = 1F;
+        [SerializeField] private float m_height = 2F;
+
+        public Vector3 Center { get { return m_center; } set { m_center = value; } }
+        public float Radius { get { return m_radius; } set { m_radius = value; } }
+        public float Height { get { return m_height; } set { m_height = value; } }
+
+        protected override NavMeshBuildSource OnBuild()
+        {
+            return new NavMeshBuildSource
+            {
+                area = Area,
+                shape = NavMeshBuildSourceShape.Capsule,
+                transform = Matrix4x4.TRS(transform.position + m_center, transform.rotation, transform.localScale),
+                size = new Vector3(m_radius * 2F, m_height, 0F),
+                component = this
+            };
+        }
+    }
+}
---
+++ Packages/UGF.Navigation/Runtime/NavMeshSourceSphereComponent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace UGF.Navigation.Runtime
+{
+    [AddComponentMenu("Unity Game Framework/Navigation/NavMesh Source Sphere", 2000)]
+    public class NavMeshSourceSphereComponent : NavMeshSourceComponent
+    {
+        [SerializeField] private Vector3 m_center;
+        [SerializeField] private float m_radius = 1F;
+
+        public Vector3 Center { get { return m_center; } set { m_center = value; } }
+        public float Radius { get { return m_radius; } set { m_radius = value; } }
+
+        protected override NavMeshBuildSource OnBuild()
+        {
+            return new NavMeshBuildSource
+            {
+                area = Area,
+                shape = NavMeshBuildSourceShape.Sphere,
+                transform = Matrix4x4.TRS(transform.position + m_center, transform.rotation, transform.localScale),
+                size = Vector3.one * m_radius * 2F,
+                component = this
+            };
+        }
+    }
+}
