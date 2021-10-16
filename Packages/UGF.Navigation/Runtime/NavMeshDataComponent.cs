96f6bc7fe7ba27c1e4b77eb02501ac269cd9aae7
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-10-16 22:08:13 +0300

    Update NavMeshDataComponent.cs

--- Packages/UGF.Navigation/Runtime/NavMeshDataComponent.cs
+++ Packages/UGF.Navigation/Runtime/NavMeshDataComponent.cs
@@ -7,11 +7,11 @@ namespace UGF.Navigation.Runtime
     [AddComponentMenu("Unity Game Framework/Navigation/NavMesh Data", 2000)]
     public class NavMeshDataComponent : MonoBehaviour
     {
+        [SerializeField] private bool m_addOnStart;
         [SerializeField] private NavMeshData m_data;
-        [SerializeField] private bool m_auto;
 
+        public bool AddOnStart { get { return m_addOnStart; } set { m_addOnStart = value; } }
         public NavMeshData Data { get { return m_data; } set { m_data = value; } }
-        public bool Auto { get { return m_auto; } set { m_auto = value; } }
         public NavMeshDataInstance Instance { get { return m_instance ?? throw new ArgumentException("Value not specified."); } }
         public bool HasInstance { get { return m_instance != null; } }
 
@@ -38,20 +38,17 @@ namespace UGF.Navigation.Runtime
             return false;
         }
 
-        private void OnEnable()
+        private void Start()
         {
-            if (m_auto && m_instance == null)
+            if (m_addOnStart)
             {
                 Add();
             }
         }
 
-        private void OnDisable()
+        private void OnDestroy()
         {
-            if (m_auto)
-            {
-                Remove();
-            }
+            Remove();
         }
     }
 }
