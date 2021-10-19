6f1feb53f5cfa073c69cc9916418b699cabf07d9
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-10-19 18:41:05 +0300

    Update NavMeshEditorInternalUtility.cs

--- Packages/UGF.Navigation/Editor/NavMeshEditorInternalUtility.cs
+++ Packages/UGF.Navigation/Editor/NavMeshEditorInternalUtility.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UGF.Navigation.Runtime;
+using UnityEditor;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
@@ -15,6 +16,20 @@ namespace UGF.Navigation.Editor
             var bounds = new Bounds(center, size);
 
             return NavMeshUtility.GetWorldBounds(bounds, component.transform.position, component.transform.rotation, component.transform.localScale);
+        }
+
+        public static void DrawWireCapsule(Vector3 position, float radius, float height)
+        {
+            Handles.DrawWireArc(position + new Vector3(0F, height - radius, 0F), Vector3.right, -Vector3.forward, 180F, radius);
+            Handles.DrawWireArc(position + new Vector3(0F, height - radius, 0F), Vector3.forward, Vector3.right, 180F, radius);
+            Handles.DrawWireDisc(position + new Vector3(0F, height - radius, 0F), Vector3.up, radius);
+            Handles.DrawWireArc(position - new Vector3(0F, height - radius, 0F), Vector3.right, -Vector3.forward, -180F, radius);
+            Handles.DrawWireArc(position - new Vector3(0F, height - radius, 0F), Vector3.forward, Vector3.right, -180F, radius);
+            Handles.DrawWireDisc(position - new Vector3(0F, height - radius, 0F), Vector3.up, radius);
+            Handles.DrawLine(position + new Vector3(radius, height - radius, 0F), position + new Vector3(radius, -(height - radius), 0F));
+            Handles.DrawLine(position + new Vector3(-radius, height - radius, 0F), position + new Vector3(-radius, -(height - radius), 0F));
+            Handles.DrawLine(position + new Vector3(0F, height - radius, radius), position + new Vector3(0F, -(height - radius), radius));
+            Handles.DrawLine(position + new Vector3(0F, height - radius, -radius), position + new Vector3(0F, -(height - radius), -radius));
         }
     }
 }
